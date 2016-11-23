264531e8b8126a7a7f1e62616872a6580efcad7e
chuong2v <chuong.2v@gmail.com>
2016-11-23 07:19:09 +0700

    app

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
 	render(){
-		return <div>Hello</div>
+    let txt = this.props.txt;
+		return <h1>{txt}</h1>
+		// return React.createElement('h1', null, "Hello Guys")
 	}
 }
-export default App;
+ReactDOM.render(
+  <App txt="this is the props value"/>,
+  document.getElementById('app')
+)
+// const App = () => <h1>Hello React</h1> 
