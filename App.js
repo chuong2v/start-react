43ddacbf61186298da6cc7d407179ed7474fb8b4
chuong2v <chuong.2v@gmail.com>
2016-11-23 07:21:58 +0700

    set properties on component

--- App.js
+++ App.js
@@ -7,8 +7,18 @@ class App extends React.Component {
 		// return React.createElement('h1', null, "Hello Guys")
 	}
 }
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'this is the default txt'
+}
+
 ReactDOM.render(
-  <App txt="this is the props value"/>,
+  <App cat={5}/>,
   document.getElementById('app')
 )
-// const App = () => <h1>Hello React</h1> 
+// const App = () => <h1>Hello React</h1>
